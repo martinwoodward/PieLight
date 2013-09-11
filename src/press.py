1e88acfa9e0015b4f9a139935ced2db671c8836e
Martin Woodward <martin@woodwardweb.com>
2013-09-11 17:00:49 +0000

    Make lights toggle as that is easier for testing

--- src/press.py
+++ src/press.py
@@ -5,12 +5,7 @@ import pifacedigitalio
 
 def switch_pressed(event):
     global pifacedigital
-    pifacedigital.output_pins[3 - event.pin_num].turn_on()
-
-
-def switch_unpressed(event):
-    global pifacedigital
-    pifacedigital.output_pins[3 - event.pin_num].turn_off()
+    pifacedigital.output_pins[3 - event.pin_num].toggle()
 
 
 if __name__ == "__main__":
@@ -22,5 +17,4 @@ if __name__ == "__main__":
     listener = pifacedigitalio.InputEventListener()
     for i in range(4):
         listener.register(i, pifacedigitalio.IODIR_ON, switch_pressed)
-        listener.register(i, pifacedigitalio.IODIR_OFF, switch_unpressed)
     listener.activate()
