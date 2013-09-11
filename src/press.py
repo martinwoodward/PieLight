0aad834ea9ddaef128f9f1f8e9e7580b37b08c84
Martin Woodward <martin@woodwardweb.com>
2013-09-11 15:52:09 +0000

    Add button press script to allow light testing

---
+++ src/press.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import pifacecommon
+import pifacedigitalio
+
+
+def switch_pressed(event):
+    global pifacedigital
+    pifacedigital.output_pins[3 - event.pin_num].turn_on()
+
+
+def switch_unpressed(event):
+    global pifacedigital
+    pifacedigital.output_pins[3 - event.pin_num].turn_off()
+
+
+if __name__ == "__main__":
+    pifacedigitalio.init()
+
+    global pifacedigital
+    pifacedigital = pifacedigitalio.PiFaceDigital()
+
+    listener = pifacedigitalio.InputEventListener()
+    for i in range(4):
+        listener.register(i, pifacedigitalio.IODIR_ON, switch_pressed)
+        listener.register(i, pifacedigitalio.IODIR_OFF, switch_unpressed)
+    listener.activate()
